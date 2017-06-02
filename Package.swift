6c5534f0d8299e4bf89b9a02a4fdb5d3aa7b994c
cardoso <matheusmcrds@gmail.com>
2017-06-02 07:08:59 -0300

    fix: unsatisfiable Package.swift

--- Package.swift
+++ Package.swift
@@ -6,7 +6,7 @@ let package = Package(
         .Package(url: "https://github.com/vapor/vapor.git",                 majorVersion: 2),
         .Package(url: "https://github.com/vapor/fluent-provider.git",       majorVersion: 1),
         .Package(url: "https://github.com/vapor/jwt.git",                   majorVersion: 2),
-        .Package(url: "https://github.com/matheusmcardoso/more-fluent.git",                   majorVersion: 2)
+        .Package(url: "https://github.com/cardoso/more-fluent.git", majorVersion: 1)
     ],
     exclude: [
         "Config",
