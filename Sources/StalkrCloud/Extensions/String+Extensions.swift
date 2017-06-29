4de7d78365098b62955edddd5aef2390340f3f05
cardoso <matheusmcrds@gmail.com>
2017-06-29 10:55:23 -0300

    feat(String.random): constrain with RandomStringOptions

--- Sources/StalkrCloud/Extensions/String+Extensions.swift
+++ Sources/StalkrCloud/Extensions/String+Extensions.swift
@@ -15,14 +15,45 @@ extension String {
     }
 }
 
+struct RandomStringOptions: OptionSet {
+    var rawValue: Set<Character>
+    
+    static let lowercase = RandomStringOptions.string("abcdefghijklmnopqrstuvwxyz")
+    static let uppercase = RandomStringOptions.string("ABCDEFGHIJKLMONPQRSTUVWXYZ")
+    static let numbers = RandomStringOptions.string("0123456789")
+    
+    static func string(_ string: String) -> RandomStringOptions {
+        return RandomStringOptions(rawValue: Set<Character>(string.characters))
+    }
+    
+    init(rawValue: Set<Character>) {
+        self.rawValue = rawValue
+    }
+    
+    init() {
+        self.rawValue = Set<Character>()
+    }
+    
+    mutating func formUnion(_ other: RandomStringOptions) {
+        self.rawValue.formUnion(other.rawValue)
+    }
+    
+    mutating func formIntersection(_ other: RandomStringOptions) {
+        self.rawValue.formIntersection(other.rawValue)
+    }
+    
+    mutating func formSymmetricDifference(_ other: RandomStringOptions) {
+        self.rawValue.formSymmetricDifference(other.rawValue)
+    }
+}
+
 extension String {
-    static func random(length: Int) -> String {
-        
+    static func random(length: Int, options: RandomStringOptions) -> String {
         var randomString = ""
-        let letters = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789"
+        let letters = options.rawValue
         
         for _ in 1...length {
-            let randomIndex  = Int(arc4random_uniform(UInt32(letters.characters.count)))
+            let randomIndex  = Int(arc4random_uniform(UInt32(letters.count)))
             let a = letters.index(letters.startIndex, offsetBy: randomIndex)
             randomString +=  String(letters[a])
         }
